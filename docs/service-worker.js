a44fb1942bb702b026d9e5ee7a769531e0e70d5e
isaiaha09 <isaiah.aris@gmail.com>
2025-11-16 10:57:26 -0800

    added jptraining videos to pre cache

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,12 @@ const APP_SHELL = [
 
   '/JPTraining/static/images/jptraining_logo.png',
   '/JPTraining/static/images/favicon-192x192.png',
-  '/JPTraining/static/images/favicon-512x512.png'
+  '/JPTraining/static/images/favicon-512x512.png',
+
+  'JPTraining/static/videos/jptraining_video1.mp4',
+  'JPTraining/static/videos/jptraining_video2.mp4',
+  'JPTraining/static/videos/jptraining_video3.mp4',
+  'JPTraining/static/videos/jptraining_video4.mp4',
 ];
 
 self.addEventListener('install', event => {
@@ -42,26 +47,24 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   const reqUrl = new URL(event.request.url);
 
-  // Cache everything under /JPTraining/static/
   if (reqUrl.pathname.startsWith('/JPTraining/static/')) {
     event.respondWith(
       caches.match(event.request).then(cacheRes => {
         if (cacheRes) return cacheRes;
 
         return fetch(event.request).then(fetchRes => {
-          // Clone the response BEFORE it's used anywhere
-          const resClone = fetchRes.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, resClone);
-          });
-
+          // Only cache successful, complete responses
+          if (fetchRes.status === 200) {
+            const resClone = fetchRes.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, resClone);
+            });
+          }
           return fetchRes;
         });
       })
     );
   } else {
-    // Default network-last strategy
     event.respondWith(
       caches.match(event.request).then(cacheRes => cacheRes || fetch(event.request))
     );
